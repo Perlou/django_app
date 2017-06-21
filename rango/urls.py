69836b21dbed6f636f9e520518e94f55c8b9586e
Perlou <a87387875@163.com>
2017-06-21 20:22:33 +0800

    feat

--- rango/urls.py
+++ rango/urls.py
@@ -14,7 +14,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
 
 if not settings.DEBUG:
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
